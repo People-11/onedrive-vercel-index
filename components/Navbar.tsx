c2e2316518922a7e6a587beb830494883fe3db74
People-11 <349504120p@gmail.com>
2022-03-11 00:01:58 +0800

    Remove search bar due to terrible search experience

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ import { Fragment, useEffect, useState } from 'react'
 import { useTranslation } from 'next-i18next'
 
 import siteConfig from '../config/site.config'
-import SearchModal from './SearchModal'
+{/*import SearchModal from './SearchModal'*/}
 import SwitchLang from './SwitchLang'
 import useDeviceOS from '../utils/useDeviceOS'
 
@@ -60,9 +60,9 @@ const Navbar = () => {
   return (
     <div className="sticky top-0 z-[100] border-b border-gray-900/10 bg-white dark:border-gray-500/30 dark:bg-gray-900">
       <Toaster />
-
+{/*
       <SearchModal searchOpen={searchOpen} setSearchOpen={setSearchOpen} />
-
+*/}
       <div className="mx-auto flex w-full items-center justify-between space-x-4 px-4 py-1">
         <Link href="/" passHref>
           <a className="flex items-center space-x-2 py-2 hover:opacity-80 dark:text-white md:p-2">
@@ -70,7 +70,7 @@ const Navbar = () => {
             <span className="hidden font-bold sm:block">{siteConfig.title}</span>
           </a>
         </Link>
-
+{/*
         <div className="flex flex-1 items-center space-x-4 text-gray-700 md:flex-initial">
           <button
             className="flex flex-1 items-center justify-between rounded-lg bg-gray-100 px-2.5 py-1.5 hover:opacity-80 dark:bg-gray-800 dark:text-white md:w-48"
@@ -81,13 +81,13 @@ const Navbar = () => {
               <span className="truncate text-sm font-medium">{t('Search ...')}</span>
             </div>
 
-            {/*<div className="hidden items-center space-x-1 md:flex">
+            <div className="hidden items-center space-x-1 md:flex">
               <div className="rounded-lg bg-gray-200 px-2 py-1 text-xs font-medium dark:bg-gray-700">
                 {os === 'mac' ? '⌘' : 'Ctrl'}
               </div>
               <div className="rounded-lg bg-gray-200 px-2 py-1 text-xs font-medium dark:bg-gray-700">K</div>
-            </div>*/}
-          </button>
+            </div>
+          </button>*/}
 
           {/*<SwitchLang />*/}
 
